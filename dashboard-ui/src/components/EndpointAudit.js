c25a059a28888e7c057bfe91806a2975a2243dc9
Chitwaan <kaurchitwan7@gmail.com>
2024-03-21 14:40:54 -0700

    updated endpointaudit for task1

--- dashboard-ui/src/components/EndpointAudit.js
+++ dashboard-ui/src/components/EndpointAudit.js
@@ -1,24 +1,28 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState,  useCallback } from 'react'
 import '../App.css';
 
 export default function EndpointAudit(props) {
     const [isLoaded, setIsLoaded] = useState(false);
     const [log, setLog] = useState(null);
     const [error, setError] = useState(null)
-	const rand_val = Math.floor(Math.random() * 100); // Get a random event from the event store
+    const [index, setIndex] = useState(null); 
+	// const rand_val = Math.floor(Math.random() * 100); // Get a random event from the event store
 
-    const getAudit = () => {
+    const getAudit = useCallback(() => {
+        const rand_val = Math.floor(Math.random() * 100); // Get a random event index
         fetch(`http://microservices-3855.eastus.cloudapp.azure.com:8110/${props.endpoint}?index=${rand_val}`)
             .then(res => res.json())
-            .then((result)=>{
-				console.log("Received Audit Results for " + props.endpoint)
+            .then((result) => {
+                console.log("Received Audit Results for " + props.endpoint);
                 setLog(result);
                 setIsLoaded(true);
-            },(error) =>{
-                setError(error)
+                setIndex(rand_val); // Set index here to ensure synchronization
+            }, (error) => {
+                setError(error);
                 setIsLoaded(true);
-            })
-    }
+            });
+    }, [props.endpoint]);
+
 	useEffect(() => {
 		const interval = setInterval(() => getAudit(), 4000); // Update every 4 seconds
 		return() => clearInterval(interval);
@@ -32,7 +36,7 @@ export default function EndpointAudit(props) {
         
         return (
             <div>
-                <h3>{props.endpoint}-{rand_val}</h3>
+                <h3>{props.endpoint}-{index}</h3>
                 {JSON.stringify(log)}
             </div>
         )
